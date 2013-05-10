d4f17e614a1318ec73a9266ede8fbad69bc6c8af
bifrost.dev <bifrost.dev@gmail.com>
2013-05-10 16:32:14 +0200

    * init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bifrost.HelloWorld
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
